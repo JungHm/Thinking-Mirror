31c854b37d566363892d23977e9241b6c6d8dc19
JungHm <taggma12@gmail.com>
2022-03-08 20:50:38 +0900

    Initial Commit

---
+++ Thinking Mirror/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thinking Mirror
+//
+//  Created by 안정흠 on 2022/03/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
