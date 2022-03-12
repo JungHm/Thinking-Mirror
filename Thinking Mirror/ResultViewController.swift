d80fbf660b19ac95aaa0a3ce848032d698b9be7f
JungHm <taggma12@gmail.com>
2022-03-12 20:24:47 +0900

    Stroyboard 수정 Navigation Controller추가 결과 화면 추가

---
+++ Thinking Mirror/ResultViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ResultViewController.swift
+//  Thinking Mirror
+//
+//  Created by 안정흠 on 2022/03/12.
+//
+
+import Foundation
+
+class ResultViewController: ViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
